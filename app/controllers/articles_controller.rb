4de69582138a574ff98704b600f24e24ffb477a3
robxspinoza <roberto.espinoza@deq.com.mx>
2019-11-28 13:19:33 -0600

    form-based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+    include ArticlesHelper
+
    def index
     @articles = Article.all
    end
@@ -7,4 +10,26 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
    end
     
+   def new
+     @article = Article.new 
+   end
+
+   def create
+    #fail
+    #fail is an instruction for see the post data
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+   end
+
+   def edit
+    @article = Article.find(params[:id])
+   end
+
+   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)       
+   end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,8 @@
 module ArticlesHelper
+    
+   #strong parameters
+   def article_params
+    params.require(:article).permit(:title, :body)
+   end
+
 end
