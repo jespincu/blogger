bb239c5064c6acb1f8bf74150443902da60080a4
robxspinoza <roberto.espinoza@deq.com.mx>
2019-11-28 14:10:53 -0600

    Tagging feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,4 +34,16 @@ class ArticlesController < ApplicationController
     flash.notice = "Article '#{@article.title}' Updated!"
     redirect_to article_path(@article)       
    end
+
+   def tag_list
+    self.tags.collect do |tag|
+        tag.name
+    end.join(", ")       
+   end
+
+   def tag_list=(tags_string)
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags          
+   end
 end
